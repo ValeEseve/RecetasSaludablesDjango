0a6a0ab7c99089babd5941f88329570c7fc661f8
Vale Eseve <vale.eseve@gmail.com>
2025-11-01 18:10:52 -0300

    Responsividad agregada

--- recetas/urls.py
+++ recetas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.lista_recetas, name='Recetas Index'),
+    path('', views.lista_recetas, name='lista_recetas'),
+    path('receta/<int:id>/', views.detalle_receta, name='detalle_receta'),
 ]
--- recetas/views.py
+++ recetas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Receta
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def lista_recetas(request):
@@ -8,3 +10,9 @@ def lista_recetas(request):
     for receta in recetas:
         receta.ingredientes_lista = [i.strip() for i in receta.ingredientes.split('•')]
     return render (request, 'receta_list.html', context)
+
+def detalle_receta(request, id):
+    receta = get_object_or_404(Receta, id=id)
+    ingredientes_lista = [i.strip() for i in receta.ingredientes.split('•')]
+    receta.ingredientes_lista = ingredientes_lista
+    return render(request, 'detalle_receta.html', {'receta': receta})
